d1840841b187215952ba80191b6073691c73e74d
Michele Bianchi <michele.bianchi@embark-studios.com>
2022-10-14 15:10:12 +0200

    Removed unused import

--- install.py
+++ install.py
@@ -15,7 +15,7 @@ from flatlaf import FlatLaf
 logger = logging.getLogger(__name__)
 
 
-def main(args: argparse.Namespace):
+def main(args: argparse.Namespace) -> int:
     """Install Ghidra dark theme
 
     Args:
@@ -30,11 +30,13 @@ def main(args: argparse.Namespace):
     try:
         if ghidra_utils.is_running():
             logger.error("Please close any running Ghidra instances.")
-            sys.exit(-1)
+            return -1
     except subprocess.SubprocessError as err:
         logger.error(f"Encountered error while checking Ghidra: {err.with_traceback}")
+        return -1
     except OSError as err:
         logger.error(f"Encountered error: {err.strerror}")
+        return err.errno
 
     ghidra_install_path: Path = args.install_path
     if not ghidra_install_path:
@@ -43,7 +45,7 @@ def main(args: argparse.Namespace):
             logger.debug("Using Ghidra install path %s", ghidra_install_path)
         except FileNotFoundError as err:
             logger.error("Could not find Ghidra installation, specify with --path")
-            sys.exit(err.errno)
+            return err.errno
 
     ghidra_version = ghidra_utils.get_version(ghidra_install_path)
     logger.debug("Found Ghidra v%s", ghidra_version)
@@ -68,10 +70,15 @@ def main(args: argparse.Namespace):
             config_handler.install_dark_preferences(ghidra_config_path)
     except URLError as err:
         logger.warning(f"Encountered an error while retrieving files: {err.strerror}")
+        return err.errno
     except OSError as err:
         logger.warning(f"Encountered an OS error: {err.strerror}")
+        return err.errno
     except AssertionError as err:
         logger.error(f"Failed an assertion: {err.with_traceback}")
+        return -1
+
+    return 0
 
 
 if __name__ == "__main__":
@@ -94,4 +101,4 @@ if __name__ == "__main__":
         "-r", "--remove", action="store_true", help="uninstall dark theme"
     )
 
-    main(parser.parse_args())
+    sys.exit(main(parser.parse_args()))
